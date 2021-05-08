56d82402da0cbf9fc8d46f4b743728299595ce89
Domene99 <jadomene99@gmail.com>
2021-05-07 19:32:21 -0500

    laboratorio 9

---
+++ l9/routes/htmlRoutes.js
@@ -0,0 +1,30 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  app.get("/", function(req, res) {
+    res.render("pages/home");
+  });
+
+  app.get("/tables", function(req, res) {
+      res.render("pages/tables", {tableData: tableData, waitListData: waitListData});
+  });
+  app.get("/reserve", function(req, res) {
+      res.render("pages/reserve");
+  });
+};
